cde3758441a3667407617c9c5e45e0f1625390cd
flyingcodd <jhonramos0101@gmail.com>
2022-11-27 00:33:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Perfil from './pages/perfil';
+
+function App() {
+  return (
+    <div className="App">
+      Inicio
+      <Perfil />
+    </div>
+  );
+}
+export default App;
